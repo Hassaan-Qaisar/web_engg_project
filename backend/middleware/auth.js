e3693c6692fb8a168dcc9d7a83b43145ac029fbe
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-11 10:31:19 +0500

    Fix the jwt token and seller cookies

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,40 +4,53 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 const Shop = require("../model/shop");
 
-exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
-    const {token} = req.cookies;
+// Middleware to check if the user is authenticated
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+    // Extract the token from cookies
+    const { token } = req.cookies;
 
-    if(!token){
+    // Check if the token is present
+    if (!token) {
         return next(new ErrorHandler("Please login to continue", 401));
     }
 
+    // Verify the token and decode the user information
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
+    // Retrieve user information from the database and attach it to the request
     req.user = await User.findById(decoded.id);
 
     next();
 });
 
+// Middleware to check if the user is a seller
+exports.isSeller = catchAsyncErrors(async (req, res, next) => {
+    // Extract the seller token from cookies
+    const { seller_token } = req.cookies;
 
-exports.isSeller = catchAsyncErrors(async(req,res,next) => {
-    const {seller_token} = req.cookies;
-    if(!seller_token){
+    // Check if the seller token is present
+    if (!seller_token) {
         return next(new ErrorHandler("Please login to continue", 401));
     }
 
+    // Verify the seller token and decode the seller information
     const decoded = jwt.verify(seller_token, process.env.JWT_SECRET_KEY);
 
+    // Retrieve seller information from the database and attach it to the request
     req.seller = await Shop.findById(decoded.id);
 
     next();
 });
 
-
+// Middleware to check if the user has admin privileges
 exports.isAdmin = (...roles) => {
-    return (req,res,next) => {
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorHandler(`${req.user.role} can not access this resources!`))
-        };
+    return (req, res, next) => {
+        // Check if the user's role is included in the allowed roles
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`${req.user.role} can not access this resource!`));
+        }
+
+        // If the user's role is allowed, proceed to the next middleware
         next();
-    }
-}
+    };
+};
