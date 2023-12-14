af3f9d87db3cb4ad27a5bcf8910b5df81a1e9d7d
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-14 10:52:44 +0500

    changes the withdraw controller

--- backend/controller/withdraw.js
+++ backend/controller/withdraw.js
@@ -7,7 +7,7 @@ const Withdraw = require("../model/withdraw");
 const sendMail = require("../utils/sendMail");
 const router = express.Router();
 
-// create withdraw request --- only for seller
+// Create withdraw request --- only for sellers
 router.post(
   "/create-withdraw-request",
   isSeller,
@@ -20,11 +20,12 @@ router.post(
         amount,
       };
 
+      // Send email notification to the seller about the withdraw request
       try {
         await sendMail({
           email: req.seller.email,
           subject: "Withdraw Request",
-          message: `Hello ${req.seller.name}, Your withdraw request of ${amount}$ is processing. It will take 3days to 7days to processing! `,
+          message: `Hello ${req.seller.name}, Your withdraw request of ${amount}$ is processing. It will take 3 days to 7 days for processing! `,
         });
         res.status(201).json({
           success: true,
@@ -33,12 +34,12 @@ router.post(
         return next(new ErrorHandler(error.message, 500));
       }
 
+      // Create a new withdraw record
       const withdraw = await Withdraw.create(data);
 
+      // Update the available balance of the seller
       const shop = await Shop.findById(req.seller._id);
-
       shop.availableBalance = shop.availableBalance - amount;
-
       await shop.save();
 
       res.status(201).json({
@@ -51,8 +52,7 @@ router.post(
   })
 );
 
-// get all withdraws --- admnin
-
+// Get all withdraw requests --- admin
 router.get(
   "/get-all-withdraw-request",
   isAuthenticated,
@@ -71,7 +71,7 @@ router.get(
   })
 );
 
-// update withdraw request ---- admin
+// Update withdraw request status ---- admin
 router.put(
   "/update-withdraw-request/:id",
   isAuthenticated,
@@ -80,6 +80,7 @@ router.put(
     try {
       const { sellerId } = req.body;
 
+      // Update the withdraw request status to "succeed"
       const withdraw = await Withdraw.findByIdAndUpdate(
         req.params.id,
         {
@@ -89,28 +90,28 @@ router.put(
         { new: true }
       );
 
+      // Update seller's transections with the details of the completed withdraw
       const seller = await Shop.findById(sellerId);
-
       const transection = {
         _id: withdraw._id,
         amount: withdraw.amount,
         updatedAt: withdraw.updatedAt,
         status: withdraw.status,
       };
-
       seller.transections = [...seller.transections, transection];
-
       await seller.save();
 
+      // Send payment confirmation email to the seller
       try {
         await sendMail({
           email: seller.email,
           subject: "Payment confirmation",
-          message: `Hello ${seller.name}, Your withdraw request of ${withdraw.amount}$ is on the way. Delivery time depends on your bank's rules it usually takes 3days to 7days.`,
+          message: `Hello ${seller.name}, Your withdraw request of ${withdraw.amount}$ is on the way. Delivery time depends on your bank's rules; it usually takes 3 days to 7 days.`,
         });
       } catch (error) {
         return next(new ErrorHandler(error.message, 500));
       }
+
       res.status(201).json({
         success: true,
         withdraw,
