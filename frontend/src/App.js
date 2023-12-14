a9693af93d8271c0b34b1199aece5c57937c247a
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-14 16:10:01 +0500

    initialize the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter} from "react-router-dom";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  
+  return (
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
+};
+
+export default App;
