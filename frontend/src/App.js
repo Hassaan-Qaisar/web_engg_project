6d217e061cc08e3f06aec1138ba3c54fde4cbd29
ZubairKhalid42 <zubairkhalidjaveed@gmail.com>
2023-12-15 11:20:05 +0500

    added route and page for home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
       <ToastContainer
