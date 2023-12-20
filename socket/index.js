43196607571552cccb451c6ad36ee65c89050e8c
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-20 13:28:04 +0500

    Add socket.io event handling for user addition, sending messages, marking messages as seen, updating and getting the last message, and user disconnection.

--- socket/index.js
+++ socket/index.js
@@ -1,3 +1,4 @@
+// Import required modules
 const socketIO = require("socket.io");
 const http = require("http");
 const express = require("express");
@@ -6,32 +7,40 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+// Load environment variables from a .env file
 require("dotenv").config({
   path: "./.env",
 });
 
+// Use middleware
 app.use(cors());
 app.use(express.json());
 
+// Simple route for testing purposes
 app.get("/", (req, res) => {
   res.send("Hello world from socket server!");
 });
 
+// Array to store connected users
 let users = [];
 
+// Function to add a user to the users array
 const addUser = (userId, socketId) => {
   !users.some((user) => user.userId === userId) &&
     users.push({ userId, socketId });
 };
 
+// Function to remove a user from the users array
 const removeUser = (socketId) => {
   users = users.filter((user) => user.socketId !== socketId);
 };
 
+// Function to get a user based on their userId
 const getUser = (receiverId) => {
   return users.find((user) => user.userId === receiverId);
 };
 
+// Function to create a message object
 const createMessage = ({ senderId, receiverId, text, images }) => ({
   senderId,
   receiverId,
@@ -40,17 +49,20 @@ const createMessage = ({ senderId, receiverId, text, images }) => ({
   seen: false,
 });
 
+// Socket.io event handling
 io.on("connection", (socket) => {
-
   console.log(`a user is connected`);
 
+  // Event to handle user addition
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
     io.emit("getUsers", users);
   });
 
-  const messages = {}; 
+  // Object to store messages
+  const messages = {};
 
+  // Event to handle sending messages
   socket.on("sendMessage", ({ senderId, receiverId, text, images }) => {
     const message = createMessage({ senderId, receiverId, text, images });
 
@@ -65,6 +77,7 @@ io.on("connection", (socket) => {
     io.to(user?.socketId).emit("getMessage", message);
   });
 
+  // Event to handle marking a message as seen
   socket.on("messageSeen", ({ senderId, receiverId, messageId }) => {
     const user = getUser(senderId);
 
@@ -73,9 +86,9 @@ io.on("connection", (socket) => {
         (message) =>
           message.receiverId === receiverId && message.id === messageId
       );
+
       if (message) {
         message.seen = true;
-
         io.to(user?.socketId).emit("messageSeen", {
           senderId,
           receiverId,
@@ -85,7 +98,7 @@ io.on("connection", (socket) => {
     }
   });
 
-  // update and get last message
+  // Event to update and get the last message
   socket.on("updateLastMessage", ({ lastMessage, lastMessagesId }) => {
     io.emit("getLastMessage", {
       lastMessage,
@@ -93,7 +106,7 @@ io.on("connection", (socket) => {
     });
   });
 
-  //when disconnect
+  // Event to handle user disconnection
   socket.on("disconnect", () => {
     console.log(`a user disconnected!`);
     removeUser(socket.id);
@@ -101,6 +114,7 @@ io.on("connection", (socket) => {
   });
 });
 
+// Start the server on the specified port or default to 4000
 server.listen(process.env.PORT || 4000, () => {
   console.log(`server is running on port ${process.env.PORT || 4000}`);
 });
